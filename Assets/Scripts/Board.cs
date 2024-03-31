964d27c6ad5af27b40c1113bb080912f608621f9
cesar <cesaroumeres@gmailcom>
2024-03-31 11:57:09 -0500

    se creo la funcion para que las piezas solo se muevan con piezas continuas de forma vertical y horizontal

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -88,7 +88,7 @@ public class Board : MonoBehaviour
 
     public void TileUp(Tile tile_)
     {
-        if(startTile!=null && endTile != null)
+        if(startTile!=null && endTile != null && IsCloseTo(startTile, endTile))
         {
             SwapTiles();
         }
@@ -106,5 +106,17 @@ public class Board : MonoBehaviour
 
         Pieces[startTile.x, startTile.y] = EndPiece;
         Pieces[endTile.x, endTile.y] = StarPiece;
+    }
+    public bool IsCloseTo(Tile start, Tile end)
+    {
+        if(Math.Abs(start.x - end.x) == 1  && start.y == end.y)
+        {
+            return true;
+        }
+        if(Math.Abs(start.y - end.y) == 1 && start.x == end.x)
+        {
+            return true;
+        }
+        return false;
     }
 }
